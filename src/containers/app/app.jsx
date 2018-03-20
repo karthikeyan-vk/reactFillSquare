0fd187bf2c64d6906bb38928afb1a101f15ef33c
karthikeyan-vk <karthi3259@gmail.com>
2018-03-20 11:16:32 +0530

    popup modal mioght be removed later

--- src/containers/app/app.jsx
+++ src/containers/app/app.jsx
@@ -5,6 +5,7 @@ import './app.css';
 
 import Quad from '../../components/quad';
 import Twitter from '../../components/twitter';
+import Popupmodel from '../../components/popupmodel';
 
 const getTweetText = game => game.current
   ? `Reached%20${game.current.number}%20on%20100%20squares%20game!`
@@ -16,7 +17,7 @@ class App extends Component {
     return (
       <div className="app">
         <header className="title">
-          <h1><span className="end">10*10</span> <span class="header">Fill Squares Game</span></h1>
+          <h1><span className="end">10*10</span> <span className="header">Fill Squares Game</span></h1>
           <p>Fill the squares with numbers from 1 to 100. </p>
           <p> Condition: If the next number to be filled diagonally skip 1 cell  or 2 cells if horizontally or vertically.</p>
         </header>
